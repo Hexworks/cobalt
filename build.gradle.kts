264bf5043db59d5aadfd1a9e3d9fc5657a698ee2
Sidem <7662187+Sidem@users.noreply.github.com>
2022-02-12 22:25:01 +0100

    attempting build fix
    adding repositories to circumvent bintray 502

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,21 @@
 allprojects {
 
     repositories {
+        
+    }
+}
+allprojects {
+    repositories {
         mavenLocal()
         mavenCentral()
         jcenter()
+        kotlinx()
+        kotlinEap()
+        jitpack()
         maven("https://dl.bintray.com/kotlin/kotlinx")
     }
+}
+
+subprojects {
+    apply<MavenPublishPlugin>()
 }
